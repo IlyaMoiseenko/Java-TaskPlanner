f3938aa30a43bcfc5d547d23275d573c5ddcf02d
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-01-19 14:09:04 +0300

    initial commit

---
+++ src/main/java/by/moiseenko/javataskplanner/JavaTaskPlannerApplication.java
@@ -0,0 +1,13 @@
+package by.moiseenko.javataskplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTaskPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTaskPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/moiseenko/javataskplanner/JavaTaskPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.moiseenko.javataskplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTaskPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
